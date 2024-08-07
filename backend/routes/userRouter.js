e721e5749d6ceee2e9046dc58f2a6331d7d9e5e6
fearalert <dhakalrohan229@gmail.com>
2024-08-07 09:17:10 +0545

    added login route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { register } from "../controllers/usersController.js";
+import { login, register } from "../controllers/usersController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
 
