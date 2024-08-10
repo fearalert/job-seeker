f022d0ce58d248ad931b67e3ccb377e4d8a4c032
fearalert <dhakalrohan229@gmail.com>
2024-08-10 19:38:48 +0545

    Created application Schema and router initiated

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import { errorMiddleware } from "./middlewares/error.js";
 import { userRouter } from "./routes/userRouter.js";
 import fileUpload from "express-fileupload";
 import { jobRouter } from "./routes/jobRouter.js";
+import { applicationRouter } from "./routes/applicationRouter.js";
 
 const app = express();
 config({path: "./config/config.env"});
@@ -27,8 +28,9 @@ app.use(fileUpload({
     tempFileDie: "/tmp/",
 }));
 
-app.use("/api/v1/user", userRouter)
-app.use("/api/v1/job", jobRouter)
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
 
 connection();
 
---
+++ backend/models/applicationSchema.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema({
+    jobSeekerInfo: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        name:{
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            validate:[validator.isEmail, "Please provide valid email."]
+        },
+        phone:{
+            type: Number,
+            required: true,
+        },
+        address:{
+            type: String,
+            required: true,
+        },
+        resume:{
+            public_id: String,
+            url: String,
+        },
+        coverLetter:{
+            public_id: String,
+            url: String,
+        },
+        role:{
+            type: String,
+            required: true,
+            enum: ["Job Seeker"]
+        },
+    },
+    employerInfo: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        role:{
+            type: String,
+            required: true,
+            enum: ["Employer"]
+        },
+    },
+    jobInfo: {
+        jobId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+          },
+          jobTitle: {
+            type: String,
+            required: true,
+          },
+    },
+    deletedBy: {
+        jobSeeker: {
+          type: Boolean,
+          default: false,
+        },
+        employer: {
+          type: Boolean,
+          default: false,
+        },
+      },
+});
+
+export const ApplicationSchema = mongoose.model("Application", applicationSchema);
