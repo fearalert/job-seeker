40c7ebf97e7f2029d80d9ebc267111e4448069e4
Rohan Dhakal <67753397+fearalert@users.noreply.github.com>
2024-11-10 23:19:56 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import JobsPage from "./pages/Jobs/JobsPage";
 import EmployeerLogin from "./pages/login/employeer/EmployeerLogin";
 import EmployeerRegister from "./pages/register/Employeer/EmployerRegister";
 import JobSeekRegister from "./pages/register/JobSeeker/JobSeekRegister";
-import JobSeekerLogin from "./pages/login/JobSeeker/JobSeekerLogin";
+import JobSeekerLogin from ".pages/login/JobSeeker/JobSeekerLogin";
 
 
 function App() {
